357c4d2692d49e57ffe76f340e43c932cd0ff110
OmarMWarraich <earthling986@gmail.com>
2026-08-09 21:28:54 +0500

    Add Devise columns and modules to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,28 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :trackable, :timeoutable
   has_many :instructed_courses, class_name: "Course", foreign_key: :instructor_id, dependent: :nullify
   has_many :enrollments, dependent: :destroy
   has_many :enrolled_courses, through: :enrollments, source: :course
   has_many :lesson_completions, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-  validates :email, :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :role, presence: true
 
   enum :role, { student: 0, instructor: 1, admin: 2 }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    if login
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 
   def full_name
     "#{first_name} #{last_name}".strip
