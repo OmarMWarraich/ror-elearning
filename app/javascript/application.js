b1e0933b378d9c66112ec441beccab08de06dd5e
OmarMWarraich <earthling986@gmail.com>
2026-08-09 21:09:43 +0500

    feat: install Action Text and Active Storage for rich content

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,6 @@ document.addEventListener("turbo:load", () => {
   document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(el => new bootstrap.Tooltip(el))
   document.querySelectorAll('[data-bs-toggle="popover"]').forEach(el => new bootstrap.Popover(el))
 })
+
+import "trix"
+import "@rails/actiontext"
