bac1220f1b0f0853995fcb18276c75c97db6654f
OmarMWarraich <earthling986@gmail.com>
2026-08-10 05:59:49 +0500

    test(enrollments): use published course two to avoid duplicate enrollment fixture

--- test/controllers/enrollments_controller_test.rb
+++ test/controllers/enrollments_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class EnrollmentsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @course = courses(:one)
+    @course = courses(:two)
+    @course.update!(status: :published)
     @student = users(:two)
     @instructor = users(:one)
   end
