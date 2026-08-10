ae98aa855d4fb109d48ffce49a28035cb1c70d1f
OmarMWarraich <earthling986@gmail.com>
2026-08-10 05:36:34 +0500

    fix(controllers): enforce CanCanCan authorization and integrate lessons, reviews, enrollments into courses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,25 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: exception.message }
+      format.json { render json: { error: exception.message }, status: :forbidden }
+    end
+  end
+
+  helper_method :current_enrollment
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :username, :first_name, :last_name ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :username, :first_name, :last_name, :bio ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email username first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email username first_name last_name bio])
+  end
+
+  def current_enrollment
+    return unless user_signed_in? && @course
+
+    @current_enrollment ||= @course.enrollments.active.find_by(user: current_user)
   end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,30 +1,33 @@
 class CoursesController < ApplicationController
-  before_action :authenticate_user!, except: %i[ index show search ]
-  before_action :set_course, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: %i[index show search]
+  before_action :set_course, only: %i[show edit update destroy]
+  before_action :authorize_course, only: %i[index new create search]
 
-  # GET /courses or /courses.json
   def index
-    @courses = Course.all
+    @courses = Course.accessible_by(current_ability).recent
   end
 
-  # GET /courses/1 or /courses/1.json
   def show
+    authorize! :read, @course
+    @lessons = @course.lessons.accessible_by(current_ability).ordered
+    @reviews = @course.reviews.order(created_at: :desc).limit(10)
+    @review = @course.reviews.new if user_signed_in? && can?(:create, Review.new(user: current_user, course: @course))
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
     @course.instructor = current_user
+    authorize! :create, @course
   end
 
-  # GET /courses/1/edit
   def edit
+    authorize! :update, @course
   end
 
-  # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
     @course.instructor ||= current_user
+    authorize! :create, @course
 
     respond_to do |format|
       if @course.save
@@ -37,8 +40,9 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
+    authorize! :update, @course
+
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: "Course was successfully updated.", status: :see_other }
@@ -50,8 +54,8 @@ class CoursesController < ApplicationController
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
   def destroy
+    authorize! :destroy, @course
     @course.destroy!
 
     respond_to do |format|
@@ -61,17 +65,21 @@ class CoursesController < ApplicationController
   end
 
   def search
-    @courses = Course.where("lower(title) LIKE ?", "%#{params[:q].to_s.downcase}%")
+    @courses = Course.accessible_by(current_ability)
+                     .where("lower(title) LIKE ?", "%#{params[:q].to_s.downcase}%")
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.expect(course: [ :title, :description, :status, :instructor_id, :category_id, :price_cents, :duration_in_minutes, :published_at ])
-    end
+  def set_course
+    @course = Course.find(params.expect(:id))
+  end
+
+  def authorize_course
+    authorize! params[:action].to_sym, Course
+  end
+
+  def course_params
+    params.expect(course: %i[title description status instructor_id category_id price_cents duration_in_minutes published_at])
+  end
 end
